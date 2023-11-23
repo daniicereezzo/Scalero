e8532837497bf818f95cf7968e61aef74dfdd6d9
Dani Cerezo <64024594+daniicereezzo@users.noreply.github.com>
2023-11-23 18:50:00 +0100

    Add HealthManager.cs

---
+++ Scalero/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    int health;
+    int maxHealth = 100;
+    bool isDead = false;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            health = 0;
+            isDead = true;
+        }
+    }
+
+    public void Heal(int heal)
+    {
+        if(isDead) { return; }
+
+        health += heal;
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    public void KillSelf()
+    {
+        if(isDead) { return; }
+
+        health = 0;
+        isDead = true;
+    }
+
+    public void Revive()
+    {
+        if(!isDead) { return; }
+
+        health = maxHealth;
+        isDead = false;
+    }
+    
+    public void RefillHealth()
+    {
+        if(isDead) { return; }
+
+        health = maxHealth;
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public int GetMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    public void SetMaxHealth(int maxHealth)
+    {
+        this.maxHealth = maxHealth;
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
