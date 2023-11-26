57ea1765c6df653c2aab6b1c4974fdfa83a576aa
kalvinzabo <javiarias2001@gmail.com>
2023-11-27 00:59:28 +0100

    Add changes and improvements in ladder
    Both prefab (add rigidbody and tag) and mostly script

--- Scalero/Assets/Scripts/LadderController.cs
+++ Scalero/Assets/Scripts/LadderController.cs
@@ -2,38 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LadderController : MonoBehaviour
+public class LadderController : MonoBehaviour, IDamageable
 {
     [SerializeField] private int numberOfSteps = 3; // Number of steps in the ladder
     [SerializeField] private const int MIN_STEPS = 3; // Minimum number of steps in the ladder
     [SerializeField] private const int MAX_STEPS = 5; // Maximum number of steps in the ladder  
     private GameObject activeLadder;
+    private Rigidbody2D rb;
     [SerializeField] private Transform handBone;
+    private bool isDead = false;
 
     private void Start()
     {
         activeLadder = transform.GetChild(numberOfSteps-MIN_STEPS).gameObject;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         //this will be called by the characterController
-        if(Input.GetKeyDown(KeyCode.O))
-        {
-            IncreaseSize();
-        }
-        if(Input.GetKeyDown(KeyCode.P))
-        {
-            DecreaseSize();
-        }
-        if(Input.GetKeyDown(KeyCode.I))
-        {
-            SetFloor();
-        }
-        if(Input.GetKeyDown(KeyCode.U))
-        {
-            SetWeapon();
-        }   
+        // if(Input.GetKeyDown(KeyCode.O))
+        // {
+        //     IncreaseSize();
+        // }
+        // if(Input.GetKeyDown(KeyCode.P))
+        // {
+        //     DecreaseSize();
+        // }
+        // if(Input.GetKeyDown(KeyCode.I))
+        // {
+        //     SetFloor();
+        // }
+        // if(Input.GetKeyDown(KeyCode.U))
+        // {
+        //     SetLadder();
+        // }   
     }
 
     void IncreaseSize()
@@ -79,12 +82,15 @@ public class LadderController : MonoBehaviour
     {
         activeLadder.GetComponent<Collider2D>().isTrigger = false;
         transform.SetParent(null);
+        rb.isKinematic = true;
     }
 
     public void SetLadder()
     {
         activeLadder.GetComponent<Collider2D>().isTrigger = true;
         transform.SetParent(null);
+        rb.isKinematic = true;
+        isDead = false;
     }
 
     public void SetWeapon()
@@ -92,6 +98,25 @@ public class LadderController : MonoBehaviour
         activeLadder.GetComponent<Collider2D>().isTrigger = true;
         transform.position = handBone.position;
         transform.SetParent(handBone);
+        rb.isKinematic = true;
+    }
+
+    public bool IsDead()
+    {
+        // if(Mathf.Approximately(transform.rotation.eulerAngles.z, 0))
+        // {   return false;}
+        // else
+        // {   return true;}
+        return isDead;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        isDead = true;
+        rb.isKinematic = false;
+        activeLadder.GetComponent<Collider2D>().isTrigger = false;
+        rb.AddForce(new Vector2(-damage, damage)*100);
+        rb.AddTorque(damage*100);
     }
 
 
