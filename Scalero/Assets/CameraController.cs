c30d96562b09b41032e96df752e85cef606f6ee4
kalvinzabo <javiarias2001@gmail.com>
2023-12-01 18:10:40 +0100

    Create Camera Controller and fix floor

---
+++ Scalero/Assets/CameraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Transform playerTransform;
+    //camera position limits
+    const float minX = -4.6f;
+    const float maxX = 4.45f;
+    const float minY = -1.54f;
+    const float maxY = 2.95f;
+    const float smoothSpeed = 0.1f;
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = new Vector3(Mathf.Clamp(playerTransform.position.x, minX, maxX), Mathf.Clamp(playerTransform.position.y, minY, maxY), transform.position.z);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
